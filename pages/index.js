dbf641b332949c77eb37f731a44605378a222403
seoseongmin96 <justin090452@gmail.com>
2022-04-28 14:29:53 +0900

    2022-04-28 14:29

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Layout } from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
